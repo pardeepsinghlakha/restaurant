141af2dab52b1c7e7a7d82af61b3a91312e27ce3
Pardeep Singh <pardeepsinghlakha@gmail.com>
2024-05-13 18:52:47 +0530

    remove usernewurlpasder and use unifiedtopology from the db file

--- db.js
+++ db.js
@@ -3,13 +3,9 @@ require("dotenv").config();
 
 //Define mongo db URI
 const DATABASE_NAME = process.env.DATABASE_NAME;
-//const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 // const database_URI_Local = process.env.DATABASE_URI_LOCAL;
 const database_URI_Online = process.env.DATABASE_URI_ONLINE;
-mongoose.connect(database_URI_Online, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(database_URI_Online);
 
 const db = mongoose.connection;
 db.on("connected", () => {
