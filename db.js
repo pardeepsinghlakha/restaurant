04a39bb040ddcb61c7c152ee46827238ca981271
Pardeep Singh <pardeepsinghlakha@gmail.com>
2024-05-13 22:04:22 +0530

    DB file code improved with comments

--- db.js
+++ db.js
@@ -1,25 +1,32 @@
-const mongoose = require("mongoose");
+// Importing dotenv to load environment variables from .env file
 require("dotenv").config();
 
-//Define mongo db URI
+// Importing mongoose for MongoDB interactions
+const mongoose = require("mongoose");
 
-// const database_URI_Local = process.env.DATABASE_URI_LOCAL;
-const database_URI_Online = process.env.DATABASE_URI_ONLINE;
-mongoose.connect(database_URI_Online, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Retrieving the MongoDB URI from environment variables
+const databaseURI = process.env.DATABASE_URI_ONLINE;
 
+// Connecting to MongoDB using the provided URI
+mongoose.connect(databaseURI);
+
+// Getting the default connection object from mongoose
 const db = mongoose.connection;
+
+// Event listener for successful MongoDB connection
 db.on("connected", () => {
-  console.log("MongoDB Database successfully Connected");
+  console.log("MongoDB Database connected successfully");
 });
+
+// Event listener for MongoDB connection error
 db.on("error", (err) => {
-  console.error("MongoDB Connection error:", err);
+  console.error("MongoDB connection error:", err);
 });
+
+// Event listener for MongoDB disconnection
 db.on("disconnected", () => {
-  console.log("MongoDB Server Disconnected ");
+  console.log("MongoDB disconnected");
 });
-module.exports = {
-  db,
-};
+
+// Exporting the MongoDB connection object for external usage
+module.exports = db;
